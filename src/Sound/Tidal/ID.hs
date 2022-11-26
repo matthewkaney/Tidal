c591f563337ecddc011bf9112e627ca3e55a80ac
Matthew Kaney <matthew@mindofmatthew.com>
2022-11-25 21:18:01 -0500

    Start sketching out proposal for scoped ids

--- src/Sound/Tidal/ID.hs
+++ src/Sound/Tidal/ID.hs
@@ -1,4 +1,6 @@
-module Sound.Tidal.ID (ID(..)) where
+{-# LANGUAGE FlexibleInstances #-}
+
+module Sound.Tidal.ID (IDScope, getIDScope, ID, toID, intToID, valueToID) where
 
 {-
     ID.hs - Polymorphic pattern identifiers
@@ -18,24 +20,45 @@ module Sound.Tidal.ID (ID(..)) where
     along with this library.  If not, see <http://www.gnu.org/licenses/>.
 -}
 
-import GHC.Exts ( IsString(..) )
+import Control.Concurrent.MVar
+
+import Sound.Tidal.Pattern
+
+newtype IDScope = IDScope { scopeNum :: Word } deriving (Eq, Ord)
+
+defaultScope :: IDScope
+defaultScope = IDScope 0
+
+nextScope :: IO (MVar IDScope)
+nextScope = newMVar (IDScope 1)
+
+getIDScope :: IO (IDScope)
+getIDScope = do scopeVar <- nextScope
+                scope <- takeMVar scopeVar
+                putMVar scopeVar (IDScope (scopeNum scope + 1))
+                return scope
 
 -- | Wrapper for literals that can be coerced to a string and used as an identifier.
 -- | Similar to Show typeclass, but constrained to strings and integers and designed
 -- | so that similar cases (such as 1 and "1") convert to the same value.
-newtype ID = ID { fromID :: String } deriving (Eq, Show, Ord, Read)
+data ID = ID IDScope String deriving (Eq, Ord)
 
+class IDLike a where
+  toID :: a -> ID
 
-noOv :: String -> a
-noOv meth = error $ meth ++ ": not supported for ids"
+instance IDLike ID where
+  toID = id
 
-instance Num ID where
-  fromInteger = ID . show
-  (+) = noOv "+"
-  (*) = noOv "*"
-  abs = noOv "abs"
-  signum = noOv "signum"
-  (-) = noOv "-"
+instance IDLike String where
+  toID = (ID $ IDScope 0)
 
-instance IsString ID where
-  fromString = ID
+instance IDLike Integer where
+  toID = (ID $ IDScope 0) . show
+
+intToID :: Integral a => a -> ID
+intToID = toID . toInteger
+
+valueToID :: Value -> Maybe ID
+valueToID (VI a) = Just (intToID a)
+valueToID (VS a) = Just (toID a)
+valueToID _ = Nothing
